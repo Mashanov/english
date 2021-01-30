8cc40f2ab2047185d13fdb8c59f702827cf203a7
Mashanov <78263664+Mashanov@users.noreply.github.com>
2021-01-30 23:05:55 +0700

    Add files via upload

---
+++ words.js
@@ -0,0 +1,121 @@
+[
+
+	{
+
+		russia: 'Туман',
+		english: 'fog',
+		img: 'https://sun9-8.userapi.com/impg/yzV-aZ-VO06REl3OLuKERSV98uGOrRZ-jZ4YQw/h_2oEI2iBow.jpg?size=2048x1367&quality=96&proxy=1&sign=00054ef77b510f07193afc9ea8b0d007&type=album'
+	},
+
+	{
+
+		russia: 'Деньги',
+		english: 'money',
+		img: 'https://sun9-41.userapi.com/impg/98pZXVAi-Ae-IdmgqIru6tYjN3GFBpFH-AeOZA/iQpbU9t2C60.jpg?size=1400x1400&quality=96&proxy=1&sign=bbbc893ececb47707d1e7d14b1f7f9a1&type=album'
+	},
+
+	{
+
+		russia: 'Небо',
+		english: 'sky',
+		img: 'https://sun9-42.userapi.com/impg/QHZxZ7jWiROa9Z5qyx5tfJ7fuK9hoUz7xFLRbQ/rfGyWz13GQU.jpg?size=1280x1024&quality=96&proxy=1&sign=3fb14645d2e1fb8f616f634874cdaf2a&type=album'
+	},
+
+	{
+
+		russia: 'Дом',
+		english: 'house',
+		img: 'https://sun9-37.userapi.com/impg/JdtvfkFa9AvmwaVIJrwkS-E3NUqoT1GTX8iIRQ/oMj3h-Oqofs.jpg?size=1920x1280&quality=96&proxy=1&sign=b409df9c966cdd255f59a5fb73494160&type=album'
+	},
+
+	{
+
+		russia: 'Еда',
+		english: 'meal',
+		img: 'https://sun4-12.userapi.com/impg/DySA0lFE-xSsnhsbW-ErRkvA309lB5Y_gADXbA/FfApU6KsyQI.jpg?size=1024x576&quality=96&proxy=1&sign=faa6762a13616f09f41ff26cca6d6512&type=album'
+	},
+
+	{
+
+		russia: 'Одежда',
+		english: 'clothes',
+		img: 'https://sun9-55.userapi.com/impg/8DMdMAfOPgcDczzB0UG63QroJni9Kc8fsap2Ng/UddRj21Og88.jpg?size=1400x1400&quality=96&proxy=1&sign=f587af15589713229b6e463af5843f12&type=album'
+	},
+
+	{
+
+		russia: 'Доктор',
+		english: 'doctor',
+		img: 'https://sun9-44.userapi.com/impg/zb2s1okCePrSCr2QLvfalPdxSk3ruOmsNCbzdw/BjuvDB7TkdA.jpg?size=640x640&quality=96&proxy=1&sign=de774342a2c397e4fbff3f74c55f09cd&type=album'
+	},
+
+	{
+
+		russia: 'Окно',
+		english: 'window',
+		img: 'https://sun9-50.userapi.com/impg/5W9W1HFntZto1NhjTR8-c7aKcQ7EGBZj1zFL-A/Ai6M59e53tk.jpg?size=768x768&quality=96&proxy=1&sign=1013ad0d052188c87d3a9d6daea29bf5&type=album'
+	},
+
+	{
+
+		russia: 'Открыто',
+		english: 'open',
+		img: 'https://sun9-25.userapi.com/impg/6ZR-1PAMm6p_rFBjQwVgK1iA6e8SJNiOShpSKQ/JsjhCef4RWc.jpg?size=800x449&quality=96&proxy=1&sign=0f213806b854b319ca40d4f9e2e26968&type=album'
+	},
+
+	{
+
+		russia: 'Автобус',
+		english: 'bus',
+		img: 'https://sun9-60.userapi.com/impg/g1KsFBdJGuP7xu6iw3Os-MwW9U4j_M56Ju9Mdg/xMBCZuEdRaE.jpg?size=1280x906&quality=96&proxy=1&sign=985275cf3a024cbc6b0806cfcd590d4e&type=album'
+	},
+
+	{
+
+		russia: 'Запрещено',
+		english: 'forbidden',
+		img: 'https://sun9-12.userapi.com/impg/Roh5nMBXrnhkbAsN2uv6oI_lp5OiS9ikeXeP-w/bnmTvFOVsvA.jpg?size=1920x1920&quality=96&proxy=1&sign=d25193fdd31de3b263a09e5e1a12cf70&type=album'
+	},
+
+	{
+
+		russia: 'Машина',
+		english: 'car',
+		img: 'https://sun9-51.userapi.com/impg/9mv0LAlCFCPpCNK4s6m4slenYXvsAAJUqcbX8w/FcnQ7eAIIeo.jpg?size=1600x1200&quality=96&proxy=1&sign=5eebae41d6944469794fc186a027c087&type=album'
+	},
+
+	{
+
+		russia: 'Цвет',
+		english: 'color',
+		img: 'https://sun9-59.userapi.com/impg/G0LVmMhA12u_Ebk-q5JG_f0fRYmaa9BzJFp4cA/zxnHTfmLWDQ.jpg?size=2560x1600&quality=96&proxy=1&sign=089e1193e68f6bc25f46e211c82defbd&type=album'
+	},
+
+	{
+
+		russia: 'Автобусная остановка',
+		english: 'bus stop',
+		img: 'https://sun9-64.userapi.com/impg/KW1L3ReJFGyUAWFtXRH8bcuKarQcaqc4QzHgQw/y86bFKZFp8I.jpg?size=1200x800&quality=96&proxy=1&sign=7d6d2ba6fd43e8e3900f4397765878e3&type=album'
+	},
+
+	{
+
+		russia: 'Телевизор',
+		english: 'tv',
+		img: 'https://sun9-66.userapi.com/impg/ig9ghYfnrTEbJgE-9EVBbCcnQTQGknImeXlZJQ/P5zFJU_k9GA.jpg?size=1920x1080&quality=96&proxy=1&sign=66801f8471a8055eebf71f0a8a8a1c73&type=album'
+	},
+
+	{
+
+		russia: 'Игра',
+		english: 'game',
+		img: 'https://sun9-35.userapi.com/impg/WDyNGg1gfsLjqynozoc2khGr1IDL1QS2Eb_BaQ/EV7-lJuCcm0.jpg?size=800x800&quality=96&proxy=1&sign=ef308a7fb989f518e67359f72c916277&type=album'
+	},
+
+	// {
+
+		// russia: '',
+		// english: '',
+		// img: ''
+	// },
+]
