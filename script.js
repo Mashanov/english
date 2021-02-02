f1ef658136b09bc4e811b75e1dc27338a37a7c99
Mashanov <78263664+Mashanov@users.noreply.github.com>
2021-02-02 11:13:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,31 @@ function up ()
 
   it('name').html(n['russia'] + ' - ' + n['english']);
   it('img').at('new', 'src', n['img']);
+  
+  setTimeout(function ()
+  {
+    
+    it('#go').css({borderTop: '3px solid #823a3a'});
+    
+    setTimeout(function ()
+    {
+
+      it('#go').css({borderRight: '3px solid #823a3a'});
+
+      setTimeout(function ()
+      {
+
+        it('#go').css({borderBottom: '3px solid #823a3a'});
+
+        setTimeout(function ()
+        {
+
+          it('#go').css({borderLeft: '3px solid #823a3a'});
+
+        }, 750);
+      }, 750);
+    }, 750);
+  }, 750);
 }
 
 up ();
